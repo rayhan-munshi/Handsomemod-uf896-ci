d8c4b8e52590adfa02e27723617b3349e1974b08
d0ng13 <lidl1214@gmail.com>
2023-05-30 21:17:00 +0800

    add some package.

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,5 +11,4 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.2.22/g' package/base-files/files/bin/config_generate
-sed -i 's/192.168.1.1/192.168.33.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.35.1/g' package/base-files/files/bin/config_generate
